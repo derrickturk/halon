4f64dc37c276a5fbda5d638325c25a172a1a01f2
derrickturk <dwt@terminusdatascience.com>
2018-05-18 15:09:26 -0500

    Begin converting Halon structures to SFD.

--- src/Language/Halon/SFD.hs
+++ src/Language/Halon/SFD.hs
@@ -12,8 +12,13 @@ module Language.Halon.SFD (
   , render
 ) where
 
+import Language.Halon
+
+import qualified Data.Set as S
 import qualified Data.Text as T
+
 import Data.Monoid ((<>))
+import Data.Maybe (fromMaybe)
 
 newtype Escaped = Escaped { getEscaped :: T.Text }
   deriving Show
@@ -44,6 +49,41 @@ data SFD =
 
 class ToSFD a where
   toSFD :: a -> SFD
+
+instance ToSFD PrimitiveType where
+  toSFD ty = Text (tag "AllowedDataType") (Escaped (sfdType ty)) where
+    sfdType Integer = "Integer"
+    sfdType Real = "Real"
+    sfdType SingleReal = "SingleReal"
+    sfdType Currency = "Currency"
+    sfdType String = "String"
+    sfdType Date = "Date"
+    sfdType Time = "Time"
+    sfdType DateTime = "DateTime"
+    sfdType Boolean = "Boolean"
+    sfdType Binary = "Binary"
+
+instance ToSFD ValueKind where
+  toSFD k = Text (tag "Type") (Escaped (sfdKind k)) where
+    sfdKind Scalar = "Scalar"
+    sfdKind Column = "Column"
+    sfdKind Table = "Table"
+
+instance ToSFD Argument where
+  toSFD (Argument name displayName ty desc) = Container (tag "Input") $
+    [ Text (tag "Name") (escape name)
+    , Text (tag "DisplayName") (escape $ fromMaybe name displayName)
+    , case desc of
+        Nothing -> Container (tag "Description") []
+        Just d -> Text (tag "Description") (escape d)
+    ] ++ typeSFD ty
+    where
+      typeSFD (ArgumentType kind Required types) =
+        [ toSFD kind ] ++ fmap toSFD (S.toList types)
+      typeSFD (ArgumentType kind Optional types) =
+        [ toSFD kind
+        , Text (tag "IsOptional") (Escaped "true")
+        ] ++ fmap toSFD (S.toList types)
 
 indent :: Int -> T.Text
 indent = flip T.replicate "  "
